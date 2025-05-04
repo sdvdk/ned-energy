47bb906cf17dd379f6a6bdbecfa197903f2c6ec7
Steven <steven@vanderkeur.com>
2025-05-04 20:28:17 +0200

    Fix: add async_setup_entry and async_unload_entry to support config entry setup

--- custom_components/ned_energy/__init__.py
+++ custom_components/ned_energy/__init__.py
@@ -1,3 +1,16 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up NED Energy from a config entry."""
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload NED Energy config entry."""
+    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
 
 
 
