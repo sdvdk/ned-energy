b9964c98e62f41060c9f691c261fde2083a1390a
Steven <steven@vanderkeur.com>
2025-06-12 21:54:19 +0200

    fix: Update deprecated async_forward_entry_setup to async_forward_entry_setups

--- custom_components/ned_energy/__init__.py
+++ custom_components/ned_energy/__init__.py
@@ -1,16 +1,16 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
+PLATFORMS = ["sensor"]
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up NED Energy from a config entry."""
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload NED Energy config entry."""
-    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
 
 
